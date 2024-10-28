beaafbfc9e8c28317f0d946b63433d227fb6f588
Varshith-Y <varshith.y@gmail.com>
2024-10-28 22:20:03 +1100

    created index.html and leet code file

---
+++ leet_53.py
@@ -0,0 +1,19 @@
+nums = [1,2,2,3,3,3]
+counter = 0
+count_dict = {}
+result=[]
+for element in nums:
+    if element in count_dict:
+        count_dict[element] += 1
+    else:
+        count_dict[element]=1
+print(count_dict)
+reversed_dict= dict(reversed(list(count_dict.items())))
+print(reversed_dict)
+for num in reversed_dict.keys():
+    counter = counter+1
+    if counter != 2+1:
+        result.append(num)
+    else:
+        break
+print(sorted(result))
